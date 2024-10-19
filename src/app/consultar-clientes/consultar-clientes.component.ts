490c25da3856e926ec86d45c81a9b4e205110520
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-10-19 15:16:56 -0300

    added ngx pagination

--- src/app/consultar-clientes/consultar-clientes.component.ts
+++ src/app/consultar-clientes/consultar-clientes.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-consultar-clientes',
   standalone: true,
-  imports: [CommonModule, NgxMaskDirective, NgxMaskPipe],
+  imports: [CommonModule, NgxMaskDirective, NgxMaskPipe, NgxPaginationModule],
   providers: [provideNgxMask()],
   templateUrl: './consultar-clientes.component.html',
   styleUrl: './consultar-clientes.component.css'
@@ -14,6 +15,8 @@ import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 export class ConsultarClientesComponent {
 
   clientes: any[] = [];
+  
+  paginador:  number = 1;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -26,5 +29,9 @@ export class ConsultarClientesComponent {
         this.clientes = resposta as any[];
       }
     })
+  }
+
+  handlePageChange(event : any) {
+    this.paginador = event;
   }
 }
